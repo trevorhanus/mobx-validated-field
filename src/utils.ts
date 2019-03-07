88c0987e6dc3f58cde8f8d6c520f034680bb9f09
Trevor Hanus <trevorhanus@gmail.com>
2019-03-06 19:15:13 -0600

    v5.0.1

--- src/utils.ts
+++
@@ -1,9 +0,0 @@
-export type orCallback = () => any;
-
-export function isOr<T>(isVal: T | undefined, orVal: T | orCallback): T {
-    if (isVal != null) {
-        return isVal;
-    } else {
-        return typeof orVal === 'function' ? orVal() : orVal;
-    }
-}
